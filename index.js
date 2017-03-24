38b9481f17fe3c233b798dfa6f166141ed1b5d1e
Ines Pucker <ines.pucker@gmx.de>
2017-03-24 14:59:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,80 @@
 function init() {
+  //dieses Object wird beim Erzeugen der Form an das template übergeben
+
+  	var recipeFormTemplatefn = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  	var recipeFormTemplate = recipeFormTemplatefn({onsubmitAction:"createRecipe();return false;"});
+  	document.getElementById("main").innerHTML += recipeFormTemplate;
+
+    Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+    Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
+
+
+  // in diesem selbsterzeugten Helper ist es wichtig "this" zu verwenden, weil der Helper nur das jeweilige Element kennt
+  //und seine Unterelemente - hier ist value ein Unterelement
+   Handlebars.registerHelper('displayIngredient', function() {
+    if(this.value!="") {
+      return new Handlebars.SafeString("<li>" + this.value + "</li>")
+    } else {
+      return null
+    }
+  })
+
+
+
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe(){
+	var recipe = {
+	recipeName: document.getElementById("name").value,
+	recipeDescription: document.getElementById("recipeDescription").value,
+	ingredients: document.getElementsByName("ingredients")
+	}
+  var recipeTemplateFn = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var recipeTemplate = recipeTemplateFn(recipe);
+  document.getElementsByTagName("main")[0].innerHTML = recipeTemplate;
+
+   }
+
+
+   function displayEditForm(){
+	var recipe = {
+	onsubmitAction:"updateRecipe();return false",
+	recipeDescription: document.getElementById("descriptionOfRecipe").innerHTML,
+	recipeName: document.getElementById("recipeName").innerHTML
+	}
+	for (i=0; i<document.querySelectorAll('ul li').length; i++){
+//hier kann man eine Zahl einfach zu einem String addieren, weil js, das so interpretiert
+var propertyName = "ingredient_"+i;
+//hier darf die Eigenschaft als String in die eckigen Klammern geschrieben werden,
+//ansonsten darf man die Eigenschaft nicht als Sting schreiben
+recipe[propertyName] = document.querySelectorAll('ul li')[i].innerHTML;
+}
+
+	var recipeEditFormTemplateFn = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+	var recipeEditFormTemplate = recipeEditFormTemplateFn(recipe)
+	document.getElementsByTagName("main")[0].innerHTML = recipeEditFormTemplate;
+
+	}
+
+
+
+
+
+
+ function updateRecipe() {
+	var recipe = {
+	recipeName: document.getElementById("name").value,
+	recipeDescription: document.getElementById("recipeDescription").value,
+	ingredients: document.getElementsByName("ingredients")
+	}
+  var recipeTemplateFn = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  var recipeTemplate = recipeTemplateFn(recipe);
+  document.getElementsByTagName("main")[0].innerHTML = recipeTemplate;
+
+   }
+   
